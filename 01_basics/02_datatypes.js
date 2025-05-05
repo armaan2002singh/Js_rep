819e2cf62befde7bbc2e7aac012f3b7d658fb1de
Armaan singh <130390550+armaan2002singh@users.noreply.github.com>
2025-05-05 06:44:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+//alert ("hey!")
+//alert(3+3) // it is used in document, if you will use this code in console of web then you will get the pop of inner written content.
+
+//console.log("armaan"); console.log("maan") this type of coding reduce the code readability
+
+//documentation - ECMA script
+
+
+// datatypes......................
+let name= "Armaan"
+let age = 12
+let isloggedin = false
+
+// number ==> 2 to 53
+//bigint 
+// string ==>""
+//boolean ==> true / flase
+//null = standalone value / representation of empty value 
+// undefine = value is not define
+// symbol ==> uniqueness
+
+
+// object 
+
+console.log(typeof"ARMAAN"); // string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
